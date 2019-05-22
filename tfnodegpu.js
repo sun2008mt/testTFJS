a3d14ef7cdf7f58f676fa6921bed3497780ee582
Marc <710641245@qq.com>
2019-05-22 18:45:55 +0800

    create program

---
+++ tfnodegpu.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Administrator on 2019/2/13 0013.
+ */
+
+// Load the package:
+// Use '@tensorflow/tfjs-node-gpu' to run with GPU.
+// Use '@tensorflow/tfjs' to run without the C++ binding.
+const tf = require('@tensorflow/tfjs-node-gpu');
+
+// Train a simple model:
+const model = tf.sequential();
+model.add(tf.layers.dense({units: 100, activation: 'relu', inputShape: [10]}));
+model.add(tf.layers.dense({units: 1, activation: 'linear'}));
+model.compile({optimizer: 'sgd', loss: 'meanSquaredError'});
+
+const xs = tf.randomNormal([100, 10]);
+const ys = tf.randomNormal([100, 1]);
+
+model.fit(xs, ys, {
+    epochs: 100,
+    callbacks: {onEpochEnd: (epoch, log) => console.log(`Epoch ${epoch}: loss = ${log.loss}`)}
+});
